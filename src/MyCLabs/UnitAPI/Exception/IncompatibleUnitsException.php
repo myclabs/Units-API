773b44a4080c829796895f9b07df3690891109a1
Matthieu Napoli <matthieu@mnapoli.fr>
2013-12-06 12:18:23 +0100

    Added IncompatibleUnitsException

---
+++ src/MyCLabs/UnitAPI/Exception/IncompatibleUnitsException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace MyCLabs\UnitAPI\Exception;
+
+use Exception;
+
+/**
+ * The units are incompatible.
+ *
+ * @author matthieu.napoli
+ */
+class IncompatibleUnitsException extends Exception
+{
+}
--- src/MyCLabs/UnitAPI/OperationService.php
+++ src/MyCLabs/UnitAPI/OperationService.php
@@ -2,6 +2,7 @@
 
 namespace MyCLabs\UnitAPI;
 
+use MyCLabs\UnitAPI\Exception\IncompatibleUnitsException;
 use MyCLabs\UnitAPI\Exception\UnknownUnitException;
 
 /**
@@ -12,11 +13,16 @@ interface OperationService
     /**
      * Returns the conversion factor between two compatible units.
      *
+     * This conversion factor allows to convert a value from unit 1 to unit 2:
+     *     Value in unit 1 * conversion factor = value in unit 2
+     *
      * @param string $unit1 Expression of the source unit.
      * @param string $unit2 Expression of the target unit.
      *
-     * @throws UnknownUnitException
-     * @return Value New value object containing the new unit and the converted value.
+     * @throws UnknownUnitException       One of the unit is unknown.
+     * @throws IncompatibleUnitsException Units are incompatible (e.g. not the same physical quantity).
+     *
+     * @return float Conversion factor.
      */
     public function getConversionFactor($unit1, $unit2);
 }
